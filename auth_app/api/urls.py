e5e67fee24b92f43b468d448f5c033b1f88842d6
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-02-14 21:19:40 +0100

    minor changes added

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegistrationView, CookieTokenObtainPairView, CookieTokenRefreshView
 
 urlpatterns = [
-    path('api/registration/', RegistrationView.as_view(), name='registration'),
-    path('api/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/register/', RegistrationView.as_view(), name='registration'),
+    path('api/login/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
 ]
