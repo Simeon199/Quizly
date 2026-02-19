f9bb9bdb161f885ff7fb90ce4526f8b1c7174ebb
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2026-02-19 21:15:55 +0100

    minor changes in settings.py to extend the validity of the tokens for testing purposes

--- core/settings.py
+++ core/settings.py
@@ -136,8 +136,8 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
 }
 
 # Static files (CSS, JavaScript, Images)
